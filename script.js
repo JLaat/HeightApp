813a3e54f8cf3b00e6a8edd029a2dbfc6e8a357d
Joni Laatikainen <jonilaatikaine@gmail.com>
2022-08-26 15:06:22 +0300

    Started coding methods for accessing user camera

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const main = () => {
     window.addEventListener("orientationchange", onAngleChange);
 }
 
+
 const onAngleChange = (event) => {
+    let angle = event.beta - 90;
+    if (angle < 0) {
+        angle = 0;
+    }
+}
+
+const getVideo = () => {
+    let videoPromise = navigator.mediaDevices.getUserMedia({video: {facingMode: {exact: "environment"}}, audio: false});
+
+    videoPromise.then(function (signal) {
+        let videoElement = document.createElement("video");
+        videoElement.srcObject = signal;
+        videoElement.play();
+    })
 
 }
